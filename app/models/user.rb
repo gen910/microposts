f10b2666f0f2239c512263a1e0b44f0288f5bfc0
gen910 <igenegi5463@gmail.com>
2016-02-07 13:17:25 +0000

    re-submitting2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-    validates :location, presence: true
+    validates :location, presence: true, length: { maximum: 20 }, on: :update
 end
