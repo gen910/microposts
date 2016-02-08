a2e5096502f0ff71d7e2a29ff960da33e5e2b027
gen910 <igenegi5463@gmail.com>
2016-02-08 10:09:03 +0000

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
-    validates :location, presence: true, length: { maximum: 20 }, on: :update
+    validates :password, length: { minimum: 6 }, presence: true, on: :create
+    validates :password, length: { minimum: 6 }, allow_nil: true, on: :update
+    validates :location, length: { maximum: 20 }
+    validates :location, presence: true,  on: :update
 end
